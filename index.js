609182d511d22fb1b1aa265657d59e97630fc24d
DLW <dlw@MacBook-Pro-de-DLW.local>
2023-04-24 23:43:18 -0400

    Configurando o target do TypeScript

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function somar(numero1, numero2) {
     return numero1 + numero2;
 }
-const meuNumero1 = 10;
-const meuNumero2 = 5;
-const resultado = somar(meuNumero1, meuNumero2);
+var meuNumero1 = 10;
+var meuNumero2 = 5;
+var resultado = somar(meuNumero1, meuNumero2);
 console.log(resultado);
---
+++ interfaces.js
@@ -0,0 +1,11 @@
+"use strict";
+// É mais comum o uso de interfaces do que criar types, é algo inclusive recomendado na documentação do TypeScript
+function getUser() {
+    return {
+        nome: "Guilherme",
+        email: "guilherme@gmail.com",
+    };
+}
+function setUser(usuario) {
+    // ...
+}
