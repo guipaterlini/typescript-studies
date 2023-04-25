706ca0196acee2a8e98741bacdb6df1cf1989c00
DLW <dlw@MacBook-Pro-de-DLW.local>
2023-04-25 00:12:49 -0400

    Herança

--- classes.ts
+++ classes.ts
@@ -35,8 +35,13 @@ class Carro {
   ) {}
 
   private alterarVelocidade(delta: number) {
-    // FACO AS VALIDAÇÕES DE ACELERACAO E FRENAGEM
-    // this.velocidadeAtual = XXX
+    const novaVelocidade = this.velocidadeAtual + delta;
+
+    if (novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima) {
+      this.velocidadeAtual = novaVelocidade;
+    } else {
+      this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+    }
   }
 
   acelerar() {
@@ -50,3 +55,22 @@ class Carro {
 
 const carro = new Carro("Chevrolet", "Onix", 160);
 carro.acelerar();
+
+// Herança
+class Camaro extends Carro {
+  private turbo = false; // nesse exemplo ao colocar o turbo como private a unica foma de modifica-lo é por dentro da propria classe usando o ligarTurbo
+
+  // usando o extends eu herdo tudo da classe Carro
+  constructor() {
+    super("Chevrolet", "Camaro", 500);
+  }
+
+  ligarTurbo() {
+    this.turbo = true;
+  }
+}
+
+const camaro = new Camaro();
+camaro.acelerar();
+camaro.frear();
+camaro.ligarTurbo();
